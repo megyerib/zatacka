b698a65fbb60f3aabe004fe35156efad723c9624
Balázs Megyeri <megyerib@users.noreply.github.com>
2021-11-22 20:50:15 +0100

    Two layer drawer

---
+++ code/two_layer_drawer.cpp
@@ -0,0 +1,54 @@
+#include "two_layer_drawer.h"
+
+TwoLayerDrawer::TwoLayerDrawer(
+    SDL_Renderer* renderer,
+    const SDL_Rect* pos_on_renderer
+) : dest_rect(*pos_on_renderer),
+    renderer(renderer)
+{
+    // Generate and draw base texture
+    base_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA32,
+        SDL_TEXTUREACCESS_TARGET,
+        dest_rect.w,
+        dest_rect.h
+    );
+
+    SDL_SetRenderTarget(renderer, base_texture);
+    DrawBase(base_texture);
+    SDL_SetRenderTarget(renderer, NULL);
+}
+
+TwoLayerDrawer::~TwoLayerDrawer()
+{
+    SDL_DestroyTexture(base_texture);
+}
+
+int TwoLayerDrawer::Draw()
+{
+    // Copy the base texture onto the renderer
+    int ret = SDL_SetRenderTarget(renderer, NULL);
+    ret = ret || SDL_RenderCopy(renderer, base_texture, NULL, &dest_rect);
+    
+    // Create overlay texture and set its blend mode
+    SDL_Texture* temp_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA32,
+        SDL_TEXTUREACCESS_TARGET,
+        dest_rect.w,
+        dest_rect.h
+    );
+    ret = ret || (temp_texture == NULL);
+    ret = ret || SDL_SetTextureBlendMode(temp_texture, SDL_BLENDMODE_BLEND);
+
+    ret = ret || SDL_SetRenderTarget(renderer, temp_texture);
+    ret = ret || DrawTemp(temp_texture);
+    ret = ret || SDL_SetRenderTarget(renderer, NULL);
+
+    ret = ret || SDL_RenderCopy(renderer, temp_texture, NULL, &dest_rect);
+
+    SDL_DestroyTexture(temp_texture);
+
+    return ret;
+}
---
+++ code/two_layer_drawer.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <SDL.h>
+
+// Use case:
+//
+// There is something you need to draw. It has two major parts. One keeps its
+// state between redraws, the other not. This base class has two virtual
+// functions which you can override. DrawBase() draws the stationery texture
+// once, and DrawTemp() draws the temporary texture every time you draw the full
+// image.
+// 
+// The constructor calls DrawBase() which draws the base texture once, then it
+// won't modify it again. Of course, child classes can modify the base texture
+// too, its purpose is only to keep its state between drawings.
+// 
+// When you call the Draw() function it copies the base texture onto the main
+// renderer, then creates a brand new temporary texture then calls the
+// DrawTemp() function to create the overlay layer from scratch, then copies it
+// onto the main renderer as well. Then the temporary layer will be destroyed,
+// you can start over the redraw in the next turn.
+
+class TwoLayerDrawer
+{
+public:
+    TwoLayerDrawer(SDL_Renderer* renderer, const SDL_Rect* pos_on_renderer);
+    ~TwoLayerDrawer();
+
+    const SDL_Rect dest_rect;
+
+    int Draw();
+
+protected:
+    SDL_Renderer* renderer;
+    SDL_Texture* base_texture;
+
+    // Draw the base layer which keeps its state between redraws. You don't need
+    // this function if your base texture is not stationary.
+    virtual int DrawBase(SDL_Texture* base) {return 0;};
+
+    // Draw everything temporary, which you can re-generate in every iteration.
+    // If you don't override this function, you probably don't need this class
+    // either.
+    virtual int DrawTemp(SDL_Texture* temp) = 0;
+};
